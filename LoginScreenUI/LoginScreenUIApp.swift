ecdb9fc78686e9f6b7f01e2420d46b9c53f77e3a
Al-Fuad <smalfuadnur@gmail.com>
2026-04-26 20:30:20 +0600

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Nur on 26/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
