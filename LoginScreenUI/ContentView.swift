fa0fbd791961af23412c77c232d08bd46b9f81b8
Al-Fuad <smalfuadnur@gmail.com>
2026-04-27 01:38:57 +0600

    Update

--- LoginScreenUI/ContentView.swift
+++ LoginScreenUI/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isLoggedin: Bool = false
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack {
+                Color.blue.ignoresSafeArea()
+                Circle()
+                    .scale(2.2)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.2)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(0.7).foregroundColor(.white.opacity(0.15))
+                VStack {
+                    Text("Login")
+                        .bold()
+                        .font(.largeTitle)
+                        .colorInvert()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 320, height: 50)
+                        .background(Color.white
+                            .opacity(0.8))
+                        .cornerRadius(12)
+                        .border(.red.opacity(0.8), width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 320, height: 50)
+                        .background(Color.white
+                            .opacity(0.8))
+                        .cornerRadius(12)
+                        .border(.red.opacity(0.8), width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .bold()
+                        .foregroundColor(.blue)
+                        .frame(width: 320, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(12)
+                        
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $isLoggedin){
+                        EmptyView()
+                    }
+                    
+                    
+                }
+            }
+        }.navigationBarHidden(true)
+    }
+    
+    func authenticateUser(username: String, password: String,){
+        if username.lowercased() == "hello" {
+            wrongUsername = 0
+            if password.lowercased() == "11223344"{
+                wrongPassword = 0
+                isLoggedin = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongPassword = 2
         }
-        .padding()
     }
 }
 
